5b9050aea0f95969e99a740b8e5a83352a619b7a
annin3 <shunta.titech@gmail.com>
2020-09-05 15:15:36 +0900

    Initial Commit

---
+++ TitechAppLite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TitechAppLite
+//
+//  Created by 中山俊太 on 2020/09/05.
+//  Copyright © 2020 annin3. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
